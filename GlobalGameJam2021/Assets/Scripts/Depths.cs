0651a065783400f70177f5d359eee7dc4fbec2e9
Theo <theo.lemoine0@gmail.com>
2021-01-31 16:00:33 +0100

    assets and things

---
+++ GlobalGameJam2021/Assets/Scripts/Depths.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Depths : MonoBehaviour
+{
+    private Camera _cam;
+    private SpriteRenderer _renderer;
+    
+    void Start()
+    {
+        _cam = Camera.main;
+        _renderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _renderer.sortingOrder = (int)_cam.WorldToScreenPoint(gameObject.transform.position).y*-1;
+    }
+}
