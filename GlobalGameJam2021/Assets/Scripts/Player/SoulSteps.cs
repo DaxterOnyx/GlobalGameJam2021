5f9800b35d6bce7da7deea58767ebbc4e892a0e5
Theo <theo.lemoine0@gmail.com>
2021-01-31 16:55:45 +0100

    added visual footsteps

---
+++ GlobalGameJam2021/Assets/Scripts/Player/SoulSteps.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+namespace Player
+{
+    public class SoulSteps : MonoBehaviour
+    {
+        [SerializeField] private GameObject footstep;
+        [SerializeField] private float distBetweenSteps = 2;
+        
+        private Vector3 lastStepPos;
+        
+        private void Update()
+        {
+            Vector3 dep = lastStepPos - transform.position;
+            
+            if (dep.sqrMagnitude > distBetweenSteps * distBetweenSteps)
+            {
+                var inst_footstep = Instantiate(footstep, transform.position, transform.rotation);
+                inst_footstep.transform.up = -dep.normalized;
+                lastStepPos = transform.position;
+            }
+        }
+    }
+}
