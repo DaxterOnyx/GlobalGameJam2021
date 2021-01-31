a5cececd8c8f9ffcc8ebc5a9c805f71fff28db98
Daxter Onyx <daxter5onyx@gmail.com>
2021-01-31 02:01:42 +0100

    Torch and UI i guess i am tired

--- GlobalGameJam2021/Assets/Scripts/Player/PlayerControl.cs
+++ GlobalGameJam2021/Assets/Scripts/Player/PlayerControl.cs
@@ -6,27 +6,32 @@ namespace Player
     [RequireComponent(typeof(Rigidbody2D))]
     public class PlayerControl : MonoBehaviour
     {
-        [SerializeField] private float speed = 1;
+        [Header("Move")] [SerializeField] private float speed = 1;
         [SerializeField] private bool constantMove = true;
         [SerializeField] private float threshold = 0.1f;
-        
+
         [SerializeField] private Animator animator;
-        [SerializeField] private Transform viewDirection; 
-        
+        [SerializeField] private Transform viewDirection;
+
         private Rigidbody2D rigid;
         private bool moving = false;
         private Camera _cam;
         public Vector2 Direction { get; private set; } = Vector2.down;
 
         public bool FreezeMoving { get; set; } = false;
-        
-        public enum Type {
+
+        public enum Type
+        {
             Chest,
             Soul
         }
+
         public Type PlayerType { get; private set; } = Type.Chest;
-        
-        
+
+        [Header("Torch")] [SerializeField] private int torhByMantra;
+        [SerializeField] private GameObject torchPrefab;
+        private int nbTorch;
+
         void Start()
         {
             rigid = GetComponent<Rigidbody2D>();
@@ -35,28 +40,42 @@ namespace Player
 
         void Update()
         {
+            UpdateMovement();
+
+            if (PlayerType == Type.Chest && Input.GetKeyDown(KeyCode.Mouse1))
+            {
+                if (nbTorch > 0)
+                {
+                    Instantiate(torchPrefab, transform.position, Quaternion.identity);
+                    nbTorch--;
+                }
+            }
+        }
+
+        private void UpdateMovement()
+        {
             if (FreezeMoving)
             {
                 rigid.velocity = Vector2.zero;
                 return;
             }
-            
+
             moving = Input.GetMouseButton(0);
 
             Vector2 dir = (Vector2) _cam.ScreenToWorldPoint(Input.mousePosition) - (Vector2) transform.position;
             Vector2 normDir = dir.normalized;
 
             viewDirection.up = normDir;
-            
+
             animator.SetBool("Walk", moving);
             animator.SetFloat("DirX", normDir.x);
             animator.SetFloat("DirY", normDir.y);
 
             Direction = constantMove ? normDir : dir;
-            
-            if (moving && dir.magnitude > threshold) 
-                rigid.velocity = Direction * speed; 
-            else 
+
+            if (moving && dir.magnitude > threshold)
+                rigid.velocity = Direction * speed;
+            else
                 rigid.velocity = Vector2.zero;
         }
 
@@ -64,6 +83,7 @@ namespace Player
         {
             animator.SetBool("Soul", true);
             PlayerType = Type.Soul;
+            nbTorch += torhByMantra;
         }
 
         public void IntoChest()
@@ -71,6 +91,5 @@ namespace Player
             animator.SetBool("Soul", false);
             PlayerType = Type.Chest;
         }
-
     }
 }
