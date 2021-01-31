08018f7df8dd49b4bb461de90646fedc280bebc7
Theo <theo.lemoine0@gmail.com>
2021-01-31 14:55:59 +0100

    souls finaly working

--- GlobalGameJam2021/Assets/Scripts/Visibility/VisibilityTest.cs
+++ GlobalGameJam2021/Assets/Scripts/Visibility/VisibilityTest.cs
@@ -12,14 +12,20 @@ namespace Visibility
         [SerializeField] private float checkFrequency = 8;
         
         private LayerMask _ignoreCollider = ~(1 << 6);
+        private Coroutine _checker;
         
         public bool IsVisible { get; private set; }
         
-        private void Start()
+        private void OnEnable()
         {
-            StartCoroutine(CheckVisibility());
+            _checker = StartCoroutine(CheckVisibility());
         }
-    
+
+        private void OnDisable()
+        {
+            StopCoroutine(_checker);
+        }
+
         IEnumerator CheckVisibility()
         {
             for (;;)
@@ -47,6 +53,7 @@ namespace Visibility
 
                             // raycast from target to viewer, so only viewer need a collider
                             var hit = Physics2D.Raycast(point, -dir, Mathf.Infinity, _ignoreCollider);
+                            // Debug.DrawLine(point, hit.point);
                             if (hit.collider == viewer.collider) visible = true;
                         }
                     }
@@ -62,7 +69,9 @@ namespace Visibility
         {
             foreach (var relativePoint in checkRelativePoints)
             {
-                Gizmos.DrawWireSphere((Vector2)transform.TransformPoint(relativePoint), 0.1f);
+                Gizmos.DrawWireSphere(
+                    transform.TransformPoint(relativePoint), 
+                    0.1f);
             }
         }
     }
